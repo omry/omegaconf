0a02df427e440c6aa913e3920843595d23f9b417
Omry Yadan <omry@fb.com>
2019-01-15 14:36:57 -0800

    1.0.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
     LONG_DESC = fh.read()
     setuptools.setup(
         name="omegaconf",
-        version="1.0.7",
+        version="1.0.8",
         author="Omry Yadan",
         author_email="omry@yadan.net",
         description="A flexible configuration library",
