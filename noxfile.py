b70fef16e7129eff2e2ef069bc152bbfac60fd23
Omry Yadan <omry@fb.com>
2020-07-07 00:21:28 -0700

    isort 5 + nox cleanup

--- noxfile.py
+++ noxfile.py
@@ -10,19 +10,20 @@ PYTHON_VERSIONS = os.environ.get(
 ).split(",")
 
 
+def deps(session):
+    session.install("--upgrade", "setuptools", "pip")
+    session.install("-r", "requirements/dev.txt", ".", silent=True)
+
+
 @nox.session(python=PYTHON_VERSIONS)
 def omegaconf(session):
-    session.install("--upgrade", "setuptools", "pip")
-    session.install("pytest", "pytest-mock")
-    session.install(".")
-
+    deps(session)
     session.run("pytest")
 
 
 @nox.session
 def docs(session):
-    session.install("sphinx", "pytest")
-    session.install(".")
+    deps(session)
     session.chdir("docs")
     session.run("sphinx-build", "-W", "-b", "doctest", "source", "build")
     session.run("sphinx-build", "-W", "-b", "html", "source", "build")
@@ -30,9 +31,7 @@ def docs(session):
 
 @nox.session(python=PYTHON_VERSIONS)
 def coverage(session):
-    session.install("--upgrade", "setuptools", "pip")
-    session.install("coverage", "pytest", "pytest-mock")
-    session.run("pip", "install", "-r", "requirements/coverage.txt", silent=True)
+    deps(session)
     session.run("coverage", "erase")
     session.run("coverage", "run", "--append", "-m", "pytest", silent=True)
     session.run("coverage", "report", "--fail-under=100")
@@ -44,9 +43,7 @@ def coverage(session):
 
 @nox.session(python=PYTHON_VERSIONS)
 def lint(session):
-    session.install("--upgrade", "setuptools", "pip")
-    session.run("pip", "install", "-r", "requirements/lint.txt", "-e", ".", silent=True)
-
+    deps(session)
     session.run("mypy", ".", "--strict", silent=True)
     session.run("isort", ".", "--check", silent=True)
     session.run("black", "--check", ".", silent=True)
@@ -61,7 +58,6 @@ def test_jupyter_notebook(session):
                 session.python, ",".join(DEFAULT_PYTHON_VERSIONS)
             )
         )
-    session.install("--upgrade", "setuptools", "pip")
+    deps(session)
     session.install("jupyter", "nbval")
-    session.run("pip", "install", ".", silent=True)
     session.run("pytest", "--nbval", "docs/notebook/Tutorial.ipynb", silent=True)
