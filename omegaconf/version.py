a3c91ead94b13fdc6192688ef48bc2c6c2a09189
Omry Yadan <omry@fb.com>
2020-07-16 11:07:31 -0700

    bump repo version

--- omegaconf/version.py
+++ omegaconf/version.py
@@ -1,6 +1,6 @@
 import sys  # pragma: no cover
 
-__version__ = "2.0.1rc10"
+__version__ = "2.0.1rc11"
 
 msg = """OmegaConf 2.0 and above is compatible with Python 3.6 and newer.
 You have the following options:
