f8d92f104ef139bdf7f9c8d7f9791e860b8069c2
Omry Yadan <omry@fb.com>
2020-11-03 15:57:50 -0800

    release 2.1.0-dev10 (#429)

--- omegaconf/version.py
+++ omegaconf/version.py
@@ -1,6 +1,6 @@
 import sys  # pragma: no cover
 
-__version__ = "2.1.0dev9"
+__version__ = "2.1.0dev10"
 
 msg = """OmegaConf 2.0 and above is compatible with Python 3.6 and newer.
 You have the following options:
