48368d81e7fec62f906dfcb73f88ff9d835bf0f9
Olivier Delalleau <507137+odelalleau@users.noreply.github.com>
2021-02-06 09:54:42 -0500

    Rename local_install -> editable_install

--- noxfile.py
+++ noxfile.py
@@ -10,27 +10,27 @@ PYTHON_VERSIONS = os.environ.get(
 ).split(",")
 
 
-def deps(session, local_install):
+def deps(session, editable_installl):
     session.install("--upgrade", "setuptools", "pip")
-    extra_flags = ["-e"] if local_install else []
+    extra_flags = ["-e"] if editable_installl else []
     session.install("-r", "requirements/dev.txt", *extra_flags, ".", silent=True)
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def omegaconf(session):
-    deps(session, local_install=False)  # ensure we test the installed version
+    deps(session, editable_installl=False)  # ensure we test the regular install
     session.run("pytest")
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def benchmark(session):
-    deps(session, local_install=True)
+    deps(session, editable_installl=True)
     session.run("pytest", "benchmark/benchmark.py")
 
 
 @nox.session
 def docs(session):
-    deps(session, local_install=True)
+    deps(session, editable_installl=True)
     session.chdir("docs")
     session.run("sphinx-build", "-W", "-b", "doctest", "source", "build")
     session.run("sphinx-build", "-W", "-b", "html", "source", "build")
@@ -38,12 +38,12 @@ def docs(session):
 
 @nox.session(python=PYTHON_VERSIONS)
 def coverage(session):
-    # For coverage, we must use the local installation because
+    # For coverage, we must use the editable installation because
     # `coverage run -m pytest` prepends `sys.path` with "." (the current
     # folder), so that the local code will be used in tests even if we set
-    # `local_install=False`. This would cause problems due to potentially
+    # `editable_installl=False`. This would cause problems due to potentially
     # missing the generated grammar files.
-    deps(session, local_install=True)
+    deps(session, editable_installl=True)
     session.run("coverage", "erase")
     session.run("coverage", "run", "--append", "-m", "pytest", silent=True)
     session.run("coverage", "report", "--fail-under=100")
@@ -55,7 +55,7 @@ def coverage(session):
 
 @nox.session(python=PYTHON_VERSIONS)
 def lint(session):
-    deps(session, local_install=True)
+    deps(session, editable_installl=True)
     session.run("mypy", ".", "--strict", silent=True)
     session.run("isort", ".", "--check", silent=True)
     session.run("black", "--check", ".", silent=True)
@@ -70,6 +70,6 @@ def test_jupyter_notebook(session):
                 session.python, ",".join(DEFAULT_PYTHON_VERSIONS)
             )
         )
-    deps(session, local_install=False)
+    deps(session, editable_installl=False)
     session.install("jupyter", "nbval")
     session.run("pytest", "--nbval", "docs/notebook/Tutorial.ipynb", silent=True)
