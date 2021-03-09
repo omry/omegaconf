37e9319ea4744e9c83cfd442356e236c455b6aac
Omry Yadan <omry@fb.com>
2021-03-09 11:36:20 -0800

    test cleanup

--- tests/test_struct.py
+++ tests/test_struct.py
@@ -1,7 +1,7 @@
 import re
 from typing import Any, Dict
 
-import pytest
+from pytest import mark, raises
 
 from omegaconf import OmegaConf
 from omegaconf.errors import ConfigKeyError
@@ -16,7 +16,7 @@ def test_struct_set_on_dict() -> None:
     c = OmegaConf.create({"a": {}})
     OmegaConf.set_struct(c, True)
     # Throwing when it hits foo, so exception key is a.foo and not a.foo.bar
-    with pytest.raises(AttributeError, match=re.escape("a.foo")):
+    with raises(AttributeError, match=re.escape("a.foo")):
         # noinspection PyStatementEffect
         c.a.foo.bar
 
@@ -24,13 +24,13 @@ def test_struct_set_on_dict() -> None:
 def test_struct_set_on_nested_dict() -> None:
     c = OmegaConf.create({"a": {"b": 10}})
     OmegaConf.set_struct(c, True)
-    with pytest.raises(AttributeError):
+    with raises(AttributeError):
         # noinspection PyStatementEffect
         c.foo
 
     assert "a" in c
     assert c.a.b == 10
-    with pytest.raises(AttributeError, match=re.escape("a.foo")):
+    with raises(AttributeError, match=re.escape("a.foo")):
         # noinspection PyStatementEffect
         c.a.foo
 
@@ -38,18 +38,18 @@ def test_struct_set_on_nested_dict() -> None:
 def test_merge_dotlist_into_struct() -> None:
     c = OmegaConf.create({"a": {"b": 10}})
     OmegaConf.set_struct(c, True)
-    with pytest.raises(AttributeError, match=re.escape("foo")):
+    with raises(AttributeError, match=re.escape("foo")):
         c.merge_with_dotlist(["foo=1"])
 
 
-@pytest.mark.parametrize("in_base, in_merged", [(dict(), dict(a=10))])
+@mark.parametrize("in_base, in_merged", [({}, {"a": 10})])
 def test_merge_config_with_struct(
     in_base: Dict[str, Any], in_merged: Dict[str, Any]
 ) -> None:
     base = OmegaConf.create(in_base)
     merged = OmegaConf.create(in_merged)
     OmegaConf.set_struct(base, True)
-    with pytest.raises(ConfigKeyError):
+    with raises(ConfigKeyError):
         OmegaConf.merge(base, merged)
 
 
@@ -59,6 +59,6 @@ def test_struct_contain_missing() -> None:
     assert "foo" not in c
 
 
-@pytest.mark.parametrize("cfg", [{}, OmegaConf.create({}, flags={"struct": True})])
+@mark.parametrize("cfg", [{}, OmegaConf.create({}, flags={"struct": True})])
 def test_struct_dict_get(cfg: Any) -> None:
     assert cfg.get("z") is None
