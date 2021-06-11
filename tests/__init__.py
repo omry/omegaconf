83e8512a0b908577ff1113d2b064739fab1256f2
Jasha <8935917+Jasha10@users.noreply.github.com>
2021-06-11 15:23:00 -0500

    more tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -213,6 +213,7 @@ class UntypedList:
 @dataclass
 class SubscriptedList:
     list: List[int] = field(default_factory=lambda: [1, 2])
+    opt_list: Optional[List[int]] = field(default_factory=lambda: [1, 2])
 
 
 @dataclass
@@ -228,6 +229,7 @@ class SubscriptedDict:
     dict_int: Dict[int, int] = field(default_factory=lambda: {123: 4})
     dict_float: Dict[float, int] = field(default_factory=lambda: {123.45: 4})
     dict_bool: Dict[bool, int] = field(default_factory=lambda: {True: 4, False: 5})
+    opt_dict_str: Optional[Dict[str, int]] = field(default_factory=lambda: {"foo": 4})
 
 
 @dataclass
--- tests/test_optional.py
+++ tests/test_optional.py
@@ -8,13 +8,23 @@ from tests import SubscriptedDict, SubscriptedList
 
 
 @mark.parametrize(
-    ("cls,key,assignment"),
+    ("cls,key,assignment,error"),
     [
-        param(SubscriptedList, "list", [None], id="list_elt"),
-        param(SubscriptedDict, "dict_str", {"key": None}, id="dict_elt"),
+        param(SubscriptedList, "list", [None], True, id="list_elt"),
+        param(SubscriptedDict, "dict_str", {"key": None}, True, id="dict_elt"),
+        param(SubscriptedList, "list", None, True, id="list"),
+        param(SubscriptedDict, "dict_str", None, True, id="dict"),
+        param(SubscriptedList, "opt_list", [None], True, id="opt_list_elt"),
+        param(SubscriptedDict, "opt_dict_str", {"key": None}, True, id="opt_dict_elt"),
+        param(SubscriptedList, "opt_list", None, False, id="opt_list"),
+        param(SubscriptedDict, "opt_dict_str", None, False, id="opt_dict"),
     ],
 )
-def test_assign_none(cls: Any, key: str, assignment: Any) -> None:
+def test_assign_none(cls: Any, key: str, assignment: Any, error: bool) -> None:
     cfg = OmegaConf.structured(cls)
-    with raises(ValidationError):
+    if error:
+        with raises(ValidationError):
+            cfg.__setattr__(key, assignment)
+    else:
         cfg.__setattr__(key, assignment)
+        assert cfg.__getattr__(key) == assignment
