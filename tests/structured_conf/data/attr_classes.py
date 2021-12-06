1c03f6979190ff523570a1a3d14bba4f18755be2
Jasha10 <8935917+Jasha10@users.noreply.github.com>
2021-12-05 18:54:15 -0600

    Port #826 to 2.1_branch: Fix `default_factory` support in structured config subclasses (#835)

--- tests/structured_conf/data/attr_classes.py
+++ tests/structured_conf/data/attr_classes.py
@@ -556,3 +556,29 @@ class UntypedList:
 class UntypedDict:
     dict: Dict = {"foo": "var"}  # type: ignore
     opt_dict: Optional[Dict] = None  # type: ignore
+
+
+class StructuredSubclass:
+    @attr.s(auto_attribs=True)
+    class ParentInts:
+        int1: int
+        int2: int
+        int3: int = attr.NOTHING  # type: ignore
+        int4: int = MISSING
+
+    @attr.s(auto_attribs=True)
+    class ChildInts(ParentInts):
+        int2: int = 5
+        int3: int = 10
+        int4: int = 15
+
+    @attr.s(auto_attribs=True)
+    class ParentContainers:
+        list1: List[int] = MISSING
+        list2: List[int] = [5, 6]
+        dict: Dict[str, Any] = MISSING
+
+    @attr.s(auto_attribs=True)
+    class ChildContainers(ParentContainers):
+        list1: List[int] = [1, 2, 3]
+        dict: Dict[str, Any] = {"a": 5, "b": 6}
--- tests/structured_conf/test_structured_config.py
+++ tests/structured_conf/test_structured_config.py
@@ -1179,3 +1179,34 @@ class TestConfigs2:
         c3 = OmegaConf.merge(c1, c2)
         with raises(ValidationError):
             c3.missing.append("xx")
+
+
+class TestStructredConfigInheritance:
+    def test_leaf_node_inheritance(self, module: Any) -> None:
+        parent = OmegaConf.structured(module.StructuredSubclass.ParentInts)
+        child = OmegaConf.structured(module.StructuredSubclass.ChildInts)
+
+        assert OmegaConf.is_missing(parent, "int1")
+        assert OmegaConf.is_missing(child, "int1")
+
+        assert OmegaConf.is_missing(parent, "int2")
+        assert child.int2 == 5
+
+        assert OmegaConf.is_missing(parent, "int3")
+        assert child.int3 == 10
+
+        assert OmegaConf.is_missing(parent, "int4")
+        assert child.int4 == 15
+
+    def test_container_inheritance(self, module: Any) -> None:
+        parent = OmegaConf.structured(module.StructuredSubclass.ParentContainers)
+        child = OmegaConf.structured(module.StructuredSubclass.ChildContainers)
+
+        assert OmegaConf.is_missing(parent, "list1")
+        assert child.list1 == [1, 2, 3]
+
+        assert parent.list2 == [5, 6]
+        assert child.list2 == [5, 6]
+
+        assert OmegaConf.is_missing(parent, "dict")
+        assert child.dict == {"a": 5, "b": 6}
