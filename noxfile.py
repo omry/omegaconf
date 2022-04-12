b73c54749028b72b96e28275608f8579e0f72f7d
Jasha <8935917+Jasha10@users.noreply.github.com>
2022-04-12 13:48:13 -0500

    cherry-pick nox-file update to fix ci

--- noxfile.py
+++ noxfile.py
@@ -71,5 +71,15 @@ def test_jupyter_notebook(session):
             )
         )
     deps(session, editable_installl=False)
-    session.install("jupyter", "nbval")
-    session.run("pytest", "--nbval", "docs/notebook/Tutorial.ipynb", silent=True)
+    # pytest pinned due to https://github.com/computationalmodelling/nbval/issues/180
+    session.install("jupyter", "nbval", "pytest<7.0.0")
+    # Ignore deprecation warnings raised by jupyter_client in Python 3.10
+    # https://github.com/jupyter/jupyter_client/issues/713
+    extra_flags = ["-Wignore::ResourceWarning"]
+    if session.python == "3.10":
+        extra_flags.append(
+            "-Wdefault:There is no current event loop:DeprecationWarning"
+        )
+    session.run(
+        "pytest", "--nbval", "docs/notebook/Tutorial.ipynb", *extra_flags, silent=True
+    )
