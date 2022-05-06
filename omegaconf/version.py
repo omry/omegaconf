aea8b44bef0e1f1cf3b08b5707bc9671f8aaa0bc
Pádraig Brady <P@draigBrady.com>
2022-05-06 15:44:43 +0100

    bump version -> 2.2.0.dev4

--- omegaconf/version.py
+++ omegaconf/version.py
@@ -1,6 +1,6 @@
 import sys  # pragma: no cover
 
-__version__ = "2.2.0.dev3"
+__version__ = "2.2.0.dev4"
 
 msg = """OmegaConf 2.0 and above is compatible with Python 3.6 and newer.
 You have the following options:
