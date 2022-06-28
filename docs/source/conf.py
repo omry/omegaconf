687e2a2718425a450bf88ff489a708248dd55931
Jasha10 <8935917+Jasha10@users.noreply.github.com>
2022-06-28 16:54:50 -0500

    Revert typo from previous commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ master_doc = "index"
 language = "en"
 
 # List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for souce files.
+# directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
 exclude_patterns = []
 
