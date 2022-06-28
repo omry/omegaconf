474e2287109ec9a07701c7f0f3b4dc0518aed032
Jasha10 <8935917+Jasha10@users.noreply.github.com>
2022-06-28 16:36:42 -0500

    Sphinx: change language to "en"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,10 +68,10 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
+# directories to ignore when looking for souce files.
 # This pattern also affects html_static_path and html_extra_path .
 exclude_patterns = []
 
