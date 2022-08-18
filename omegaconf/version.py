319796cc9a118eb745571cc959947b02cab7565e
Jasha <8935917+Jasha10@users.noreply.github.com>
2022-08-18 11:31:44 -0500

    OmegaConf 2.2.3 release

--- omegaconf/version.py
+++ omegaconf/version.py
@@ -1,6 +1,6 @@
 import sys  # pragma: no cover
 
-__version__ = "2.2.3.dev0"
+__version__ = "2.2.3"
 
 msg = """OmegaConf 2.0 and above is compatible with Python 3.6 and newer.
 You have the following options:
