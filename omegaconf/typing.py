3f8132219f01398286509885c37667cfb5e5f512
Javier López Peña <jlopezpena@users.noreply.github.com>
2023-11-21 17:21:58 +0000

    Add comment about type alias

--- omegaconf/typing.py
+++ omegaconf/typing.py
@@ -7,4 +7,12 @@ else:
 
 from .vendor.antlr4.ParserRuleContext import ParserRuleContext
 
+# The antlr4 class `ParserRulerContext` is not a valid type for `mypy` for a number of
+# reasons, including lack of strict typing in antlr4-python3-runtime and dynamically
+# generated attributes that can only be checked at runtime.
+# To be able to use the class for type hinting without raising `valid-type` mypy errors,
+# we define a type alias that we can use throughout the code.
+# Note that type aliases cannot shadow the name of the class they are aliasing,
+# so we need to name the aliased class something different from `ParserRuleContext`
+
 Antlr4ParserRuleContext: TypeAlias = ParserRuleContext  # type: ignore[valid-type]
